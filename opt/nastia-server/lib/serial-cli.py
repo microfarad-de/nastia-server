6ca4147e7b895fcaedf09a71ee8effb8ce087e8d
Karim Hraibi <khraibi@gmail.com>
2023-05-21 07:23:19 +0000

    lib/serial-cli: Handle ilock exception

--- opt/nastia-server/lib/serial-cli.py
+++ opt/nastia-server/lib/serial-cli.py
@@ -35,39 +35,42 @@ import signal
 
 # Read the contents of the receive buffer
 def read():
-    rx = " "
-    result = ""
-    while len(rx) > 0:
-        rx = ser.readline().decode()
-        result = result + rx
-    return result
+  rx = " "
+  result = ""
+  while len(rx) > 0:
+    rx = ser.readline().decode()
+    result = result + rx
+  return result
 
 # Write to the transmit buffer
 def write(str):
-    ser.write(str.encode())
+  ser.write(str.encode())
 
 # Handle Ctrl+C
 def signal_handler(sig, frame):
-    global terminate 
-    terminate = True
-    print ("\nInterrupted by user\n")
-    time.sleep(0.3)
-    thread.join();
-    raise SystemExit(0)
+  global terminate 
+  terminate = True
+  print ("\nInterrupted by user\n")
+  time.sleep(0.3)
+  thread.join();
+  raise SystemExit(0)
 
 # Run receiving loop as a thread
 def rx_thread():
-    global terminate
-    while 1:
-        sema.acquire()
-        with lock:
-            rx = read()
-        sema.release()
-        time.sleep(0.1)
-        if rx:
-            sys.stdout.write(rx)
-        if terminate:
-            break
+  global terminate
+  while 1:
+    sema.acquire()
+    try:
+      with lock:
+        rx = read()
+    except:
+      continue
+    sema.release()
+    time.sleep(0.1)
+    if rx:
+      sys.stdout.write(rx)
+    if terminate:
+      break
 
 
 #################
@@ -75,45 +78,45 @@ def rx_thread():
 #################
 if __name__=='__main__':
 
-    print("\nInteractive Serial Console\n")
+  print("\nInteractive Serial Console\n")
 
-    # Handle Ctrl+C
-    signal.signal(signal.SIGINT, signal_handler)
+  # Handle Ctrl+C
+  signal.signal(signal.SIGINT, signal_handler)
 
-    # Check for correct number of arguments
-    if len(sys.argv) < 2:
-        print("Usage: " + sys.argv[0] + " DEVICE [BAUD_RATE]\n")
-        sys.exit()
+  # Check for correct number of arguments
+  if len(sys.argv) < 2:
+    print("Usage: " + sys.argv[0] + " DEVICE [BAUD_RATE]\n")
+    sys.exit()
 
-    DEVICE    = sys.argv[1] # RS232 device name
-    if len(sys.argv) < 3:
-        BAUD_RATE = 9600
-    else:
-        BAUD_RATE = sys.argv[2] # Serial baud rate
+  DEVICE = sys.argv[1] # RS232 device name
+  if len(sys.argv) < 3:
+    BAUD_RATE = 9600
+  else:
+    BAUD_RATE = sys.argv[2] # Serial baud rate
 
-    # System-wide lock ensures mutually exclusive access to the serial port
-    lock = ilock.ILock(DEVICE, timeout=600)
+  # System-wide lock ensures mutually exclusive access to the serial port
+  lock = ilock.ILock(DEVICE, timeout=600)
 
-    # Initialize the serial port
+  # Initialize the serial port
+  with lock:
+    ser = serial.Serial(DEVICE, BAUD_RATE, timeout=0.1)
+    print("Connected to " + str(DEVICE) + " at " + str(BAUD_RATE) + " baud")
+    print("Waiting for user input (press Ctrl+C to exit)...\n")
+
+  # Run receive routine as a thread
+  terminate = False
+  sema = Semaphore()
+  thread = Thread(target = rx_thread)
+  thread.start()
+
+  while 1:
+    tx = sys.stdin.readline()
+    sema.acquire()
     with lock:
-        ser = serial.Serial(DEVICE, BAUD_RATE, timeout=0.1)
-        print("Connected to " + str(DEVICE) + " at " + str(BAUD_RATE) + " baud")
-        print("Waiting for user input (press Ctrl+C to exit)...\n")
-
-    # Run receive routine as a thread
-    terminate = False
-    sema = Semaphore()
-    thread = Thread(target = rx_thread)
-    thread.start()
-
-    while 1:
-        tx = sys.stdin.readline()
-        sema.acquire()
-        with lock:
-            write(tx)
-        sema.release()
-        time.sleep(0.1)
-        if terminate:
-            break
+      write(tx)
+    sema.release()
+    time.sleep(0.1)
+    if terminate:
+      break
 
 
