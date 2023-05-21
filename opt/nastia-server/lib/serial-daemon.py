df01af68a49e20c8985c28fa33dbdb8628e12226
Karim Hraibi <khraibi@gmail.com>
2023-05-21 15:40:28 +0000

    bin/bt-daemon: Remove /dev/ from device name

--- opt/nastia-server/lib/serial-daemon.py
+++ opt/nastia-server/lib/serial-daemon.py
@@ -60,7 +60,7 @@ def error_log ( text ):
 
 # Read the contents of the receive buffer
 def read():
-  global DEVICE
+  global DEV
   global ser
   rx = " "
   result = ""
@@ -71,20 +71,20 @@ def read():
       time.sleep (0.1)
     except:
       #print(traceback.format_exc())
-      error_log("Failed to read from " + DEVICE)
+      error_log("Failed to read from " + DEV)
       exit_failure ()
   return result
 
 
 # Write to the transmit buffer
 def write(str):
-  global DEVICE
+  global DEV
   global ser
   try:
     ser.write(str.encode())
   except:
     #print(traceback.format_exc())
-    error_log("Failed to write to " + DEVICE)
+    error_log("Failed to write to " + DEV)
     exit_failure ()
 
 
@@ -122,17 +122,18 @@ if __name__=='__main__':
     print("Usage: " + sys.argv[0] + " DEVICE LOG [BAUD_RATE]\n")
     sys.exit()
 
-  DEVICE = str(sys.argv[1]) # Serial device name
-  LOG    = sys.argv[2] # Log file prefix
+  DEV_SHORT = str(sys.argv[1]).replace("/dev/", "")  # Serial device name without the /dev prefix
+  DEV       = "/dev/" + DEV_SHORT                    # Serial device name
+  LOG       = sys.argv[2]                            # Log file prefix
 
   if len(sys.argv) < 4:
     BAUD_RATE = 9600
   else:
-    BAUD_RATE = sys.argv[2] # Serial baud rate
+    BAUD_RATE = sys.argv[2]  # Serial baud rate
 
-  in_file     = "/tmp/serial-daemon-in"     + DEVICE.replace("/", "-")
-  out_file    = "/tmp/serial-daemon-out"    + DEVICE.replace("/", "-")
-  status_file = "/tmp/serial-daemon-status" + DEVICE.replace("/", "-")
+  in_file     = "/tmp/serial-daemon-in-"     + DEV_SHORT
+  out_file    = "/tmp/serial-daemon-out-"    + DEV_SHORT
+  status_file = "/tmp/serial-daemon-status-" + DEV_SHORT
   print ("Input file: ", in_file)
   print ("Output file:", out_file)
   print ("Status file:", status_file)
@@ -144,15 +145,15 @@ if __name__=='__main__':
 
 
   # System-wide lock ensures mutually exclusive access to the serial port
-  lock = ilock.ILock(DEVICE, timeout=600)
+  lock = ilock.ILock(DEV, timeout=600)
 
   # Initialize the serial port
   try:
     with lock:
-      ser = serial.Serial(DEVICE, BAUD_RATE, timeout=0.1)
-    info_log ("Connected to " + DEVICE + " at " + str(BAUD_RATE) + " baud")
+      ser = serial.Serial(DEV, BAUD_RATE, timeout=0.1)
+    info_log ("Connected to " + DEV + " at " + str(BAUD_RATE) + " baud")
   except:
-    error_log("Failed to connect to " + DEVICE)
+    error_log("Failed to connect to " + DEV)
     exit_failure ()
 
 
