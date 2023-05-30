5e0c2e50009ace1042c0b996844977362a195584
Karim Hraibi <khraibi@gmail.com>
2023-05-30 08:52:22 +0000

    lib/serial-daemon.py: Refactoring

--- opt/nastia-server/lib/serial-daemon.py
+++ opt/nastia-server/lib/serial-daemon.py
@@ -34,40 +34,40 @@ import os
 import traceback
 
 # Current directory where this script is located
-DIR = os.path.dirname(os.path.abspath(__file__))
+dir = os.path.dirname(os.path.abspath(__file__))
 
 
 # Print info log message
 def info_log(text):
-    global LOG
+    global log
     print(text)
-    os.popen(DIR + "/infoLog.sh \"" + text + "\" '" + LOG + "' '' 'cd'")
+    os.popen(dir + "/infoLog.sh \"" + text + "\" '" + log + "' '' 'cd'")
 
 
 # Print warning log message
 def warning_log(text):
-    global LOG
+    global log
     print("[WARNING] " + text)
-    os.popen(DIR + "/warningLog.sh \"" + text + "\" '" + LOG + "' '' 'cd'")
+    os.popen(dir + "/warningLog.sh \"" + text + "\" '" + log + "' '' 'cd'")
 
 
 # Print error log message
 def error_log(text):
-    global LOG
+    global log
     print("[ERROR] " + text)
-    os.popen(DIR + "/errorLog.sh \"" + text + "\" '" + LOG + "' '' 'cd'")
+    os.popen(dir + "/errorLog.sh \"" + text + "\" '" + log + "' '' 'cd'")
 
 
 # Print transmit/receive log message
 def trx_log(text):
-    global LOG_TRX
+    global log_trx
     print(text)
-    os.popen(DIR + "/infoLog.sh \"\n" + text + "\" '" + LOG_TRX + "' '' 'd'")
+    os.popen(dir + "/infoLog.sh \"\n" + text + "\" '" + log_trx + "' '' 'd'")
 
 
 # Read the contents of the receive buffer
 def read():
-    global DEV
+    global dev
     global ser
     rx = " "
     result = ""
@@ -78,20 +78,20 @@ def read():
             time.sleep(0.1)
         except:
             #print(traceback.format_exc())
-            error_log("Failed to read from " + DEV)
+            error_log("Failed to read from " + dev)
             exit_failure()
     return result
 
 
 # Write to the transmit buffer
 def write(str):
-    global DEV
+    global dev
     global ser
     try:
         ser.write(str.encode())
     except:
         #print(traceback.format_exc())
-        error_log("Failed to write to " + DEV)
+        error_log("Failed to write to " + dev)
         exit_failure()
 
 
@@ -145,23 +145,23 @@ if __name__ == '__main__':
 
     # Check for correct number of arguments
     if len(sys.argv) < 3:
-        print("Usage: " + sys.argv[0] + " DEVICE LOG [BAUD_RATE]\n")
+        print("Usage: " + sys.argv[0] + " device log [baud rate]\n")
         sys.exit(1)
 
-    DEV_SHORT = str(sys.argv[1]).replace(
+    dev_short = str(sys.argv[1]).replace(
         "/dev/", "")  # Serial device name without the /dev prefix
-    DEV = "/dev/" + DEV_SHORT  # Serial device name
-    LOG = sys.argv[2]  # Log file prefix
-    LOG_TRX = LOG + "-" + DEV_SHORT  # Transmit/receive log
+    dev = "/dev/" + dev_short  # Serial device name
+    log = sys.argv[2]  # Log file prefix
+    log_trx = log + "-" + dev_short  # Transmit/receive log
 
     if len(sys.argv) < 4:
-        BAUD_RATE = 9600
+        baud_rate = 9600
     else:
-        BAUD_RATE = sys.argv[2]  # Serial baud rate
+        baud_rate = sys.argv[2]  # Serial baud rate
 
-    in_file = "/tmp/serial-daemon-in-" + DEV_SHORT
-    out_file = "/tmp/serial-daemon-out-" + DEV_SHORT
-    status_file = "/tmp/serial-daemon-status-" + DEV_SHORT
+    in_file = "/tmp/serial-daemon-in-" + dev_short
+    out_file = "/tmp/serial-daemon-out-" + dev_short
+    status_file = "/tmp/serial-daemon-status-" + dev_short
     print("Input file: ", in_file)
     print("Output file:", out_file)
     print("Status file:", status_file)
@@ -172,15 +172,15 @@ if __name__ == '__main__':
         pass
 
     # System-wide lock ensures mutually exclusive access to the serial port
-    lock = ILockE(DEV, timeout=600)
+    lock = ILockE(dev, timeout=600)
 
     # Initialize the serial port
     try:
         with lock:
-            ser = serial.Serial(DEV, BAUD_RATE, timeout=0.1)
-        info_log ("Connected to " + DEV + " at " + str(BAUD_RATE) + " baud")
+            ser = serial.Serial(dev, baud_rate, timeout=0.1)
+        info_log ("Connected to " + dev + " at " + str(baud_rate) + " baud")
     except:
-        error_log("Failed to connect to " + DEV)
+        error_log("Failed to connect to " + dev)
         exit_failure ()
 
     while 1:
